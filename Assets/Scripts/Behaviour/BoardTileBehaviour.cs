123e036c8e98144c118ea6e909424a2945b28ab1
Isaiah Mann <imann24@gmail.com>
2016-05-05 15:52:18 -0400

    Adding board tile object

---
+++ Assets/Scripts/Behaviour/BoardTileBehaviour.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BoardTileBehaviour : EnvironmentalObjectBehaviour {
+
+	protected override void FetchReferences () {
+
+    }
+
+    protected override void SetReferences () {
+
+    }
+
+    protected override void HandleNamedEvent (string eventName) {
+
+    }
+}
