a12a930c9f637efeaab3bba3da45617bed27415b
imann24 <contact@isaiahmann.com>
2016-07-24 22:48:44 -0700

    Adding WorldController class

--- Assets/Scripts/World/IWorldController.cs
+++ Assets/Scripts/World/IWorldController.cs
@@ -4,10 +4,10 @@
  */
 
 public interface IWorldController : IController {
-     void Create();
-     // Cleans up/destroys the world
-     void Teardown();
-     void AddObject(IWorldObject element);
+    void Create();
+    // Cleans up/destroys the world
+    void Teardown();
+    void AddObject(IWorldObject element);
 	void RemoveObject(IWorldObject element);
-     IWorldObject GetObject(string id);
+    IWorldObject GetObject(string id);
 }
---
+++ Assets/Scripts/World/WorldController.cs
@@ -0,0 +1,43 @@
+﻿/*
+ * Author(s): Isaiah Mann
+ * Description: Controls the set up of the game world
+ */
+
+public class WorldController : MannBehaviour, IWorldController {
+	public void Create() {
+
+	}
+
+	// Cleans up/destroys the world
+	public void Teardown() {
+
+	}
+
+	public void AddObject(IWorldObject element) {
+
+	}
+
+	public void RemoveObject(IWorldObject element) {
+
+	}
+
+	public IWorldObject GetObject(string id) {
+		throw new System.NotImplementedException();
+	}
+
+	protected override void SetReferences () {
+
+	}
+
+	protected override void FetchReferences () {
+
+	}
+
+	protected override void CleanupReferences () {
+
+	}
+
+	protected override void HandleNamedEvent (string eventName) {
+
+	}
+}
