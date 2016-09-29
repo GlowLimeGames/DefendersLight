e9dd0a0103da008ef1c35822669d9661ba1b9e50
dkhopez <fallenaran@gmail.com>
2016-09-29 14:32:50 -0400

    Add files via upload
    Added Static List in TowerController to track all Towers
    Added Method to Heal Towers in CheatController
    Added Method to Get Towers in List in TowerController
    Modified Method Add/Remove Tower in TowerController to Add towers to list as well

---
+++ Assets/Scripts/Cheat/Main/TowerController.cs
@@ -0,0 +1,125 @@
+﻿/*
+ * Author(s): Isaiah Mann
+ * Description: Controls all the towers in the game
+ */
+
+using UnityEngine;
+using SimpleJSON;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.EventSystems;
+
+public class TowerController : UnitController<ITower, Tower, TowerList>, ITowerController {
+	public const string TOWER_TAG = "Tower";
+	const float DRAG_HEIGHT_OFFSET = 1.25f;
+
+	public GameObject CoreOrbPrefab;
+	public GameObject CoreOrbInstance;
+	GameObject potentialPurchaseTower = null;
+	MapTileBehaviour previousHighlightedMapTile = null;
+	HashSet<TowerBehaviour> activeTowers = new HashSet<TowerBehaviour>();
+	public static List<TowerBehaviour> healTowers = new List<TowerBehaviour> (); //9/29/16
+
+	public override void Setup (WorldController worldController, DataController dataController, string unitTemplateJSONPath) {
+		base.Setup(worldController, dataController, unitTemplateJSONPath);
+	}
+		
+	public void HandleBeginDragPurchase (PointerEventData dragEvent, TowerPurchasePanel towerPanel) {
+		if (potentialPurchaseTower != null) {
+			Destroy(potentialPurchaseTower);
+		}
+		potentialPurchaseTower = (GameObject)Instantiate(worldController.GetTowerPrefab(towerPanel.TowerType));
+		potentialPurchaseTower.GetComponent<TowerBehaviour>().ToggleColliders(false);
+	}
+
+	public void HandleDragPurchase (PointerEventData dragEvent, TowerPurchasePanel towerPanel) {
+		Vector3 dragPosition = dragEvent.position;
+		dragPosition.z = towerPanel.transform.position.z - Camera.main.transform.position.z;
+		dragPosition = Camera.main.ScreenToWorldPoint(dragPosition);
+		dragPosition.y = DRAG_HEIGHT_OFFSET;
+		potentialPurchaseTower.transform.position = dragPosition;
+		HighlightSpotToPlace(dragPosition);
+	}
+
+	void HighlightSpotToPlace (Vector3 dragPosition) {
+		RaycastHit hit;
+		if (Physics.Raycast(dragPosition, Vector3.down, out hit)) {
+			MapTileBehaviour mapTile;
+			if (hit.collider != null && (mapTile = hit.collider.GetComponent<MapTileBehaviour>()) != null) {
+				if (previousHighlightedMapTile) {
+					previousHighlightedMapTile.Unhighlight();
+				}
+				if (!mapTile.HasAgent()) {
+					mapTile.HightlightToPlace(potentialPurchaseTower.GetComponent<StaticAgentBehaviour>());
+					previousHighlightedMapTile = mapTile;
+				}
+			}
+		}
+	}
+
+	public void AddActiveTower (TowerBehaviour tower) {
+		if (!activeTowers.Contains(tower)) {
+			activeTowers.Add(tower);
+			healTowers.Add (tower);	//9/29/16
+		}
+	}
+
+	public void RemoveActiveTower (TowerBehaviour tower) {
+		if (activeTowers.Contains(tower)) {
+			activeTowers.Remove(tower);
+			healTowers.Add (tower);	//9/29/16
+		}
+	}
+
+	public void RefreshIlluminations () {
+		foreach (TowerBehaviour tower in activeTowers) {
+			if (tower is IlluminationTowerBehaviour) {
+				worldController.SendIlluminationToMap(tower as IlluminationTowerBehaviour);
+			}
+		}
+	}
+
+	public void HandleEndDragPurchase (PointerEventData dragEvent, TowerPurchasePanel towerPanel) {
+		if (previousHighlightedMapTile && !previousHighlightedMapTile.HasAgent()) {
+			previousHighlightedMapTile.PlaceAgent(potentialPurchaseTower.GetComponent<StaticAgentBehaviour>());
+			towerPanel.OnPurchased();
+		} else {
+			// TODO: Collect in object pool instead of destroying
+			Destroy(potentialPurchaseTower);
+		}
+		potentialPurchaseTower = null;
+	}
+
+	public static TowerController Instance;
+
+	public Tower[] GetActive() {
+		throw new System.NotImplementedException ();
+	}
+
+	public void Create(Tower unit) {
+		throw new System.NotImplementedException();
+	}
+
+	public void Destroy(Tower unit) {
+		throw new System.NotImplementedException();
+	}
+
+	public Tower[] GetAll() {
+		throw new System.NotImplementedException();
+	}
+
+	public static List<TowerBehaviour> GetTowers(){
+		return healTowers;
+	}
+
+	protected override void SetReferences() {
+		if (!SingletonUtil.TryInit(ref Instance, this, gameObject)) {
+			Destroy(gameObject);
+		}
+	}
+
+	protected override void CleanupReferences () {
+		base.CleanupReferences ();
+		Instance = null;
+	}
+}
