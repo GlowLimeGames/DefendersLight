3462bab7cd7b2d1a0e41934c05cb9fa411aabd94
Isaiah Mann <contact@isaiahmann.com>
2016-10-05 17:46:54 -0400

    Connecting player data to the start screen

--- Assets/Scripts/Data/Main/PlayerData.cs
+++ Assets/Scripts/Data/Main/PlayerData.cs
@@ -7,6 +7,7 @@
 public class PlayerData : IPlayerData {
 	int _xp;
 	int _level;
+	int _highestWave;
 	string _filePath;
 	public int IXP {
 		get {
@@ -18,7 +19,11 @@ public class PlayerData : IPlayerData {
 			return _level;
 		}
 	}
-
+	public int IHighestWave {
+		get {
+			return _highestWave;
+		}
+	}
 	public string IFilePath {
 		get {
 			return _filePath;
@@ -33,15 +38,17 @@ public class PlayerData : IPlayerData {
 		Reset();
 	}
 
-	public PlayerData (string filePath, int xp, int level) {
+	public PlayerData (string filePath, int xp, int level, int highestWave) {
 		setFilePath(filePath);
 		this._xp = xp;
 		this._level = level;
+		this._highestWave = highestWave;
 	}
 
 	public void Reset () {
 		this._xp = 0;
 		this._level = 1;
+		this._highestWave = 0;
 	}
 
 	void setFilePath (string filePath) {
--- Assets/Scripts/UserInterface/StartUI/StartUIController.cs
+++ Assets/Scripts/UserInterface/StartUI/StartUIController.cs
@@ -4,12 +4,27 @@
  */
 
 using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class StartUIController : UIController {
+	[SerializeField]
+	Text highestWave;
+	[SerializeField]
+	Text playerLevel;
+
 	protected override void SetReferences () {
 		base.SetReferences ();
 		SceneController.ReportSceneLoadComplete();
+		setText();
+	}
+
+	void setText () {
+		DataController data = DataController.Instance;
+		if (data) {
+			highestWave.text = string.Format(highestWave.text, data.HighestWave);
+			playerLevel.text = string.Format(playerLevel.text, data.PlayerLevel);
+		}
 	}
 
 	protected override void FetchReferences () {
