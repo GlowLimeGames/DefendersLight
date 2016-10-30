75b8ea8020b02121cafa6596d775d16248bf9e18
Isaiah Mann <contact@isaiahmann.com>
2016-10-29 22:23:43 -0400

    Implementing basic tutorial

--- Assets/Scripts/Global/Main/Templates/Controller.cs
+++ Assets/Scripts/Global/Main/Templates/Controller.cs
@@ -4,7 +4,12 @@
  */
 
 using UnityEngine;
+using UnityEngine.UI;
 
 public abstract class Controller : MannBehaviour {
-
+	protected void toggleCanvasGroup (CanvasGroup canvas, bool isActive) {
+		canvas.alpha = isActive ? 1 : 0;
+		canvas.interactable = isActive;
+		canvas.blocksRaycasts = isActive;
+	}
 }
---
+++ Assets/Scripts/Tutorial/TutorialController.cs
@@ -0,0 +1,137 @@
+﻿/*
+ * Author(s): Isaiah Mann
+ * Description: Loads and runs the tutorial
+ */
+
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections.Generic;
+
+public class TutorialController : SingletonController<TutorialController> {
+	public const string INTRO_TUTORIAL_KEY = "IntroductoryTutorial";
+	DataController data;
+	WorldController world;
+	CanvasGroup canvas;
+
+	[SerializeField]
+	bool autoStartIntroTutorial;
+
+	[SerializeField]
+	Image stepImage;
+	[SerializeField]
+	Text stepTitle;
+	[SerializeField]
+	Text stepBody;
+	[SerializeField]
+	Button previousButton;
+	[SerializeField]
+	Button nextButton;
+	[SerializeField]
+	Image nextButtonIcon;
+	[SerializeField]
+	Sprite closeIcon;
+	[SerializeField]
+	Sprite arrowIcon;
+
+	string[] TutorialKeys = new string[]{INTRO_TUTORIAL_KEY};
+	Dictionary<string, Tutorial> tutorials = new Dictionary<string, Tutorial>();
+	Tutorial current;
+	public bool HasActiveTutorial {
+		get {
+			return current != null;
+		}
+	}
+
+	void UpdateCurrentTutorial () {
+		UpdateTutorial(current);
+	}
+
+	public void UpdateTutorial (Tutorial tutorial) {
+		TutorialStep currentStep = tutorial.CurrentStep;
+		Sprite currentImage = tutorial.GetCurrentSprite();
+		stepTitle.text = currentStep.Title;
+		stepImage.sprite = currentImage;
+		stepBody.text = currentStep.Body;
+		previousButton.interactable = tutorial.HasPrevious();
+		nextButtonIcon.sprite = tutorial.HasNext() ? arrowIcon : closeIcon;
+	}
+
+	public void PlayTutorial (string key) {
+		if (tutorials.TryGetValue(key, out current)) {
+			Show();
+			current.Reset();
+			UpdateCurrentTutorial();
+			if (world) {
+				world.Pause();
+			}
+		}
+	}
+
+	public void Next (bool autoEndIfNoNext = true) {
+		if (HasActiveTutorial && current.HasNext()) {
+			current.NextStep();
+			UpdateCurrentTutorial();
+		} else if (autoEndIfNoNext) {
+			endTutorial();
+		}
+	}
+
+	public void SkipTutorial () {
+		endTutorial();
+	}
+
+	void endTutorial () {
+		current = null;
+		Close();
+		if (world) {
+			world.Resume();
+		}
+	}
+
+	public void Close () {
+		toggleCanvasGroup(canvas, false);
+		gameObject.SetActive(false);
+	}
+
+	public void Show () {
+		gameObject.SetActive(true);
+		toggleCanvasGroup(canvas, true);
+	}
+
+	public void Previous () {
+		if (HasActiveTutorial && current.HasPrevious()) {
+			current.PreviousStep();
+			UpdateCurrentTutorial();
+		}
+	}
+
+	protected override void SetReferences () {
+		base.SetReferences ();
+		canvas = GetComponent<CanvasGroup>();
+	}
+
+	protected override void FetchReferences () {
+		if (isSingleton) {
+			base.FetchReferences ();
+			data = DataController.Instance;
+			world = WorldController.Instance;
+			tutorials = loadTutorials(false, TutorialKeys);
+			if (autoStartIntroTutorial) {
+				PlayTutorial(INTRO_TUTORIAL_KEY);
+			}
+		}
+	}
+
+	Dictionary<string, Tutorial> loadTutorials (bool overwrite, params string[] keys) {
+		Dictionary<string, Tutorial> tutorials = new Dictionary<string, Tutorial>();
+		foreach (string key in keys) {
+			Tutorial tutorial = JsonUtility.FromJson<Tutorial>(data.RetrieveJSONFromResources(key));
+			if (!tutorials.ContainsKey(key)) {
+				tutorials.Add(key, tutorial);
+			} else if (overwrite) {
+				tutorials[key] = tutorial;
+			}
+		}
+		return tutorials;
+	}
+}
