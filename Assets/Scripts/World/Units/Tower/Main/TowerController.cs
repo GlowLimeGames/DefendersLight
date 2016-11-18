1fe455992f789ab2afef7cd5f262976ae5d3786c
Masterdawg10 <Masterdawg10@DESKTOP-9E3CKEE>
2016-11-17 22:43:33 -0500

    update

--- Assets/Scripts/World/Units/Tower/Main/TowerController.cs
+++ Assets/Scripts/World/Units/Tower/Main/TowerController.cs
@@ -14,7 +14,8 @@ using System.Text.RegularExpressions;
 
 public class TowerController : UnitController<ITower, Tower, TowerList>, ITowerController {
 	public const string TOWER_TAG = "Tower";
-	const float DRAG_HEIGHT_OFFSET = 1.25f;
+	const float DRAG_HEIGHT_OFFSET = 3f;
+	CameraController gameCamera;
 
 	public GameObject CoreOrbPrefab;
 	public GameObject CoreOrbInstance;
@@ -22,10 +23,18 @@ public class TowerController : UnitController<ITower, Tower, TowerList>, ITowerC
 	MapTileBehaviour previousHighlightedMapTile = null;
 	HashSet<TowerBehaviour> activeTowers = new HashSet<TowerBehaviour>();
 	Dictionary<TowerType, List<Tower>> towerTemplatesByType = new Dictionary<TowerType, List<Tower>>();
-
-	public override void Setup (WorldController worldController, DataController dataController, string unitTemplateJSONPath) {
-		base.Setup(worldController, dataController, unitTemplateJSONPath);
+	public Dictionary<TowerType, List<Tower>> ITowerTemplatesByType {
+		get {
+			return towerTemplatesByType;
+		}
+	}
+	public override void Setup (WorldController worldController, DataController dataController, MapController mapController, string unitTemplateJSONPath) {
+		base.Setup(worldController, dataController, mapController, unitTemplateJSONPath);
 		towerTemplatesByType = sortTowers(templateUnits.Values.ToArray());
+	}
+
+	public void SellTower (Tower tower) {
+		worldController.OnSellTower(tower);
 	}
 
 	public void PlaceCoreOrb (MapTileBehaviour mapTile) {
@@ -33,7 +42,7 @@ public class TowerController : UnitController<ITower, Tower, TowerList>, ITowerC
 		CoreOrbInstance = coreOrb;
 		CoreOrbBehaviour coreOrbBehaviour = coreOrb.GetComponent<CoreOrbBehaviour>();
 		coreOrbBehaviour.SetTower(templateUnits[CoreOrbBehaviour.CORE_ORB_KEY]);
-		mapTile.PlaceAgent(coreOrbBehaviour, false);
+		mapTile.PlaceStaticAgent(coreOrbBehaviour, false);
 	}
 
 	Dictionary<TowerType, List<Tower>> sortTowers (Tower[] towers) {
@@ -66,23 +75,60 @@ public class TowerController : UnitController<ITower, Tower, TowerList>, ITowerC
 		if (potentialPurchaseTower != null) {
 			Destroy(potentialPurchaseTower);
 		}
-		potentialPurchaseTower = Instantiate(worldController.GetTowerPrefab(towerPanel.TowerType)).GetComponent<TowerBehaviour>();
-		potentialPurchaseTower.ToggleColliders(false);
-		potentialPurchaseTower.SetTower(towerPanel.GetTower());
+		Vector3 startPosition = getDragPosition(dragEvent, towerPanel.transform.position);
+		this.potentialPurchaseTower = GetTowerBehaviourFromTower(towerPanel.GetTower(), startPosition, false);
 	}
 
+	public TowerBehaviour GetTowerBehaviourFromTower (Tower tower, Vector3 startPosition, bool shouldStartActive = false) {
+		TowerBehaviour potentialPurchaseTower = SpawnTower(tower, startPosition);
+		potentialPurchaseTower.Setup(this);
+		potentialPurchaseTower.ToggleColliders(shouldStartActive);
+		potentialPurchaseTower.SetTower(tower);
+		potentialPurchaseTower.ToggleActive(shouldStartActive);
+		potentialPurchaseTower.OnSpawn();
+		return potentialPurchaseTower;
+	}
+
+	public TowerBehaviour GetPrefab (Tower tower) {
+		return loadPrefab(FileUtil.CreatePath(TOWER_TAG, PREFABS_DIR, tower.Type)) as TowerBehaviour;
+	}
+
+	TowerBehaviour SpawnTower (Tower tower, Vector3 startingPosition) {
+		ActiveObjectBehaviour behaviour;
+		if (TryGetActiveObject(tower.IType, startingPosition, out behaviour)) {
+			return behaviour as TowerBehaviour;
+		} else {
+			TowerBehaviour prefabInResources = GetPrefab(tower);
+			if (prefabInResources) {
+				return Instantiate(prefabInResources);
+			} else {
+				return Instantiate(worldController.GetTowerPrefab(tower.TowerType)).GetComponent<TowerBehaviour>();
+			}
+		}
+	}
+		
 	public void HandleDragPurchase (PointerEventData dragEvent, TowerPurchasePanel towerPanel) {
-		Vector3 dragPosition = dragEvent.position;
-		dragPosition.z = towerPanel.transform.position.z - Camera.main.transform.position.z;
+		potentialPurchaseTower.transform.position = getDragPosition(dragEvent, towerPanel.transform.position);
+		HighlightSpotToPlace(potentialPurchaseTower.transform.position);
+	}
+
+	Vector3 getDragPosition (PointerEventData pointerEvent, Vector3 panelPosition) {
+		Vector3 dragPosition = pointerEvent.position;
+		dragPosition.z = panelPosition.z - Camera.main.transform.position.z;
 		dragPosition = Camera.main.ScreenToWorldPoint(dragPosition);
-		dragPosition.y = DRAG_HEIGHT_OFFSET;
-		potentialPurchaseTower.transform.position = dragPosition;
-		HighlightSpotToPlace(dragPosition);
+		// dragPosition.y = DRAG_HEIGHT_OFFSET;
+		float angle = gameCamera.ICameraAngleRad;
+		Vector3 offset = new Vector3(
+			0,
+			-Mathf.Cos(angle) * DRAG_HEIGHT_OFFSET,
+			Mathf.Sin(angle) * DRAG_HEIGHT_OFFSET
+		);
+		return dragPosition + offset;
 	}
 
 	void HighlightSpotToPlace (Vector3 dragPosition) {
 		RaycastHit hit;
-		if (Physics.Raycast(dragPosition, Vector3.down, out hit)) {
+		if (Physics.Raycast(dragPosition, gameCamera.ICameraDirection, out hit)) {
 			MapTileBehaviour mapTile;
 			if (hit.collider != null && (mapTile = hit.collider.GetComponent<MapTileBehaviour>()) != null) {
 				if (previousHighlightedMapTile) {
@@ -116,9 +162,37 @@ public class TowerController : UnitController<ITower, Tower, TowerList>, ITowerC
 		}
 	}
 
+    public void compareTowerLevels() {
+
+        
+
+        foreach (Tower tower in templateUnits.Values)
+        {
+            
+            if (tower.UnlockLevel == dataController.PlayerLevel)
+            {
+
+                
+                TowerUnlockedScreen.towerUnlocked = true;
+                TowerUnlockedScreen.textToDisplay = "You got it fam.";
+
+
+                }
+
+            
+
+        }
+
+    }
+
+	public override void HandleObjectDestroyed (ActiveObjectBehaviour activeObject) {
+		base.HandleObjectDestroyed (activeObject);
+		activeTowers.Remove(activeObject as TowerBehaviour);
+	}
+
 	public void HandleEndDragPurchase (PointerEventData dragEvent, TowerPurchasePanel towerPanel) {
-		if (previousHighlightedMapTile && !previousHighlightedMapTile.HasAgent()) {
-			previousHighlightedMapTile.PlaceAgent(potentialPurchaseTower);
+		if (previousHighlightedMapTile && previousHighlightedMapTile.CanPlaceTower()) {
+			previousHighlightedMapTile.PlaceStaticAgent(potentialPurchaseTower);
 			towerPanel.OnPurchased();
 		} else {
 			// TODO: Collect in object pool instead of destroying
@@ -176,6 +250,11 @@ public class TowerController : UnitController<ITower, Tower, TowerList>, ITowerC
 		}
 	}
 
+	protected override void FetchReferences () {
+		base.FetchReferences ();
+		gameCamera = CameraController.Instance;
+	}
+
 	protected override void CleanupReferences () {
 		base.CleanupReferences ();
 		Instance = null;
@@ -185,6 +264,6 @@ public class TowerController : UnitController<ITower, Tower, TowerList>, ITowerC
 		// Remove special characters from tower key (to produce filename)
 		Regex rgx = new Regex("[^a-zA-Z0-9 -]");
 		towerKey = rgx.Replace(towerKey, "");
-		return Resources.Load<Sprite>(Path.Combine(TOWER_TAG, towerKey.ToLower().Replace(" ", string.Empty)));
+		return Resources.Load<Sprite>(Path.Combine(TOWER_TAG, (Path.Combine(SPRITES_DIR, towerKey.ToLower().Replace(" ", string.Empty)))));
 	}
 }
