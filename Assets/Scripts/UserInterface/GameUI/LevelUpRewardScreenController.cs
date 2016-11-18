5e29ff2bf9512aa24b64cb2b55015873dba04477
Isaiah Mann <contact@isaiahmann.com>
2016-11-17 23:37:20 -0500

    Fixing singleton ref

--- Assets/Scripts/UserInterface/GameUI/LevelUpRewardScreenController.cs
+++ Assets/Scripts/UserInterface/GameUI/LevelUpRewardScreenController.cs
@@ -11,19 +11,21 @@ public class LevelUpRewardScreenController : UIController {
 
     public static LevelUpRewardScreenController Instance;    
 
-    void Start()
-    {
+	protected override void SetReferences () {
+		base.SetReferences ();
+		Instance = this;
+	}
+
+    protected override void FetchReferences () {
         newCanvas.SetActive(false);        
         
     }
 
-    public void toggleScreen()
-    {
+    public void toggleScreen() {
         StartCoroutine(toggler());               
     }
 
-    IEnumerator toggler()
-    {
+    IEnumerator toggler() {
         newCanvas.SetActive(true);
         text.text = textToDisplay;
         yield return new WaitForSeconds(secondsToWait);
